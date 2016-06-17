7719e6ac5ecce12048645c3f40ccb8427c0d2e29
Jack Williams <jack@tagstr.co>
2016-06-17 13:38:43 +0100

    First big test.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function Microboot (options) {
-    this.options = options
-
+function Microboot () {
     return this
 }
 
 Microboot.prototype.up = require('./lib/up')
+Microboot.prototype.options = require('./lib/options')
 
 module.exports = new Microboot()
