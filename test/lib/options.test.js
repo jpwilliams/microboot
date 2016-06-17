d178ad2a321a4f008d93efb06a3b448c4f82cb97
Jack Williams <jack@tagstr.co>
2016-06-17 14:19:04 +0100

    Adding pending tests for our options function

---
+++ test/lib/options.test.js
@@ -0,0 +1,6 @@
+var options = require('../../lib/options')
+
+describe('microboot/lib/options', function () {
+    it('should return the "options" function')
+    it('should set "opts" to whatever is given')
+})
