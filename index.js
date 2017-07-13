eba55811aef311c54dabe06f6e796305ecdbb4d7
Jack Williams <jpwilliamsphotography@gmail.com>
2017-07-13 16:12:14 +0100

    :boom: Brand new version using a class. Simpler and supports promises.

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-var up = require('./lib/up')
-up.up = up
+const Microboot = require('./lib/Microboot')
 
-module.exports = up
+function microboot (stages = [], arg, callback) {
+  if (!arg) {
+    arg = {}
+  } else if (!callback && typeof arg === 'function') {
+    callback = arg
+    arg = {}
+  }
+
+  if (callback && typeof callback !== 'function') {
+    throw new Error('Callback passed but not a function', callback)
+  }
+
+  const microboot = new Microboot(stages, arg)
+  const boot = microboot.boot()
+
+  if (!callback) {
+    return boot
+  }
+
+  boot.then((arg) => callback(null, arg)).catch(callback)
+}
+
+module.exports = microboot
